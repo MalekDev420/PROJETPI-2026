8cce41d551150fc68be732f9643389ce808ae56a
Malek Ben Kacem <malek.benkacem@esprit.tn>
2026-07-12 22:40:16 +0100

    autorisation cors a localhost

--- src/main/java/com/gestionmissions/config/CorsConfig.java
+++ src/main/java/com/gestionmissions/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig {
   public WebMvcConfigurer corsConfigurer() {
     return new WebMvcConfigurer() {
       @Override public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200")
+        registry.addMapping("/api/**").allowedOrigins("http://localhost","http://localhost:4200", "http://127.0.0.1:4200")
           .allowedMethods("GET","POST","PUT","DELETE","OPTIONS").allowedHeaders("*");
       }
     };
